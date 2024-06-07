c8baaab5e20b7ad5d1834fb25565feb3b0bfceba
kTrzcinskii <kt.trzcinski@gmail.com>
2024-06-07 14:41:42 +0200

    return simple message

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
-use std::net::TcpListener;
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    server_loop(listener);
+}
 
+fn server_loop(listener: TcpListener) {
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                handle_request(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_request(mut stream: TcpStream) {
+    stream
+        .write_all(b"HTTP/1.1 200 OK\r\n\r\n")
+        .expect("write_all should not fail (for now)");
 }
