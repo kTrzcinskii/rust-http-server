02eb133547c1d2c8e83b6f7d17e3ea13c9325764
kTrzcinskii <kt.trzcinski@gmail.com>
2024-06-10 19:37:18 +0200

    Split headers into request and response ones

--- src/header.rs
+++ src/header.rs
@@ -7,22 +7,38 @@ pub struct Header {
     value: String,
 }
 
-pub enum HeaderType {
+pub enum ResponseHeaderType {
     ContentType,
     ContentLength,
 }
 
-impl fmt::Display for HeaderType {
+pub enum RequestHeaderType {
+    Host,
+    UserAgent,
+    Accept,
+}
+
+impl fmt::Display for ResponseHeaderType {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            HeaderType::ContentType => write!(f, "Content-Type"),
-            HeaderType::ContentLength => write!(f, "Content-Length"),
+            ResponseHeaderType::ContentType => write!(f, "Content-Type"),
+            ResponseHeaderType::ContentLength => write!(f, "Content-Length"),
+        }
+    }
+}
+
+impl fmt::Display for RequestHeaderType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            RequestHeaderType::Host => write!(f, "Host"),
+            RequestHeaderType::UserAgent => write!(f, "User-Agent"),
+            RequestHeaderType::Accept => write!(f, "Accept"),
         }
     }
 }
 
 impl Header {
-    pub fn new(header_type: HeaderType, header_value: &str) -> Self {
+    pub fn new<T: fmt::Display>(header_type: T, header_value: &str) -> Self {
         Header {
             key: header_type.to_string(),
             value: String::from(header_value),
